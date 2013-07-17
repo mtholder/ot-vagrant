192851486fb1c67cd606abbfc5fe40e71dbec8bd
Mark T. Holder <mtholder@gmail.com>
2013-07-17 10:27:31 -0700

    updated to the most recent versionof ott

--- env_with_urls.sh
+++ env_with_urls.sh
@@ -2,7 +2,7 @@
 # This file is addresses that will change frequently to avoid frequently changing the bootstrapping scripts.
 export PHYLOGRAFTER_DB_DUMP_NAME=phylografter.20130611.sql.bz2
 export URL_FOR_PHYLOGRAFTER_DB_DUMP="http://reelab.net/~rree/${PHYLOGRAFTER_DB_DUMP_NAME}"
-export OTT_WITH_VERSION_NAME=ott2.0
+export OTT_WITH_VERSION_NAME=ott2.1
 export URL_FOR_OTT_DUMP="http://dev.opentreeoflife.org/${OTT_WITH_VERSION_NAME}/${OTT_WITH_VERSION_NAME}.tgz"
 export PHYLOGRAFTER_DB_INSTALLED=1
 export URL_FOR_NEO4J="http://download.neo4j.org/artifact?edition=community&version=1.9&distribution=tarball&dlid=1611116"
